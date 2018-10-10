b9586fc98eec1a360ab26e4e51a10a61149dd530
Eduardo Sanchez <eduardosanchez01@gmail.com>
2018-10-09 21:36:16 -0300

    primer commit

---
+++ modal.js
@@ -0,0 +1,6 @@
+
+let modal = document.getElementById("modal");
+let fondoModal = document.getElementById("fondoModal");
+let botonAbrirModal = document.getElementById("abrirModal");
+// let botonCerrarModal = ???
+
