4a51a27c93945d60789cc934045f9dcc927aafb4
Eduardo Sanchez <eduardosanchez01@gmail.com>
2018-10-09 21:41:28 -0300

    Agregue comentarios

--- modal.js
+++ modal.js
@@ -1,6 +1,9 @@
+// Variables básicas para manipular el DOM
 
 let modal = document.getElementById("modal");
 let fondoModal = document.getElementById("fondoModal");
 let botonAbrirModal = document.getElementById("abrirModal");
 // let botonCerrarModal = ???
+
+// Acá tenemos que agregar eventos que se ejecuten con el clic.
 
