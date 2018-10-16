e4bff047d92e6d36d4ed2ccfa92b9a6741b98b58
eduardoequis <38509405+eduardoequis@users.noreply.github.com>
2018-10-16 12:19:03 -0300

    Resultados actualizados

--- modal.js
+++ modal.js
@@ -3,7 +3,22 @@
 let modal = document.getElementById("modal");
 let fondoModal = document.getElementById("fondoModal");
 let botonAbrirModal = document.getElementById("abrirModal");
-// let botonCerrarModal = ???
+let botonCerrarModal = document.getElementById("cerrarModal");
 
 // Acá tenemos que agregar eventos que se ejecuten con el clic.
+
+botonAbrirModal.addEventListener ("click", function(){
+    modal.classList.toggle("cerrado");
+   fondoModal.classList.toggle("cerrado");
+})
+
+botonCerrarModal.addEventListener ("click", function(){
+    modal.classList.toggle("cerrado");
+   fondoModal.classList.toggle("cerrado");
+})
+
+fondoModal.addEventListener ("click", function(){
+    modal.classList.toggle("cerrado");
+   fondoModal.classList.toggle("cerrado");
+})
 
